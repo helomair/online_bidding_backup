fc2cf9216629781c56ea75652206b83a9c43a33c
mengru <4momolmr@gmail.com>
2019-05-14 15:33:18 +0800

    新增頁面-上傳商品

--- public/前端競標平台/js/scripts.js
+++ public/前端競標平台/js/scripts.js
@@ -1,61 +1,112 @@
-function add_news() {
-  var table = document.getElementById("news_table");
-  var news = document.getElementById("news");
-  var page = document.getElementById("pagination");
-  var footer = document.getElementById("footer");
-  var num = document.getElementById("news_table").rows.length;
-  console.log(table);
-  table.classList.add("display-none");
-  page.classList.add("display-none");
+function edit_news() {
+        var table = document.getElementById("news_table");
+        var news = document.getElementById("news");
+        var page = document.getElementById("pagination");
+        var footer = document.getElementById("footer");
 
-   var text = document.createElement("textarea");
-   text.name = "post";
-   text.maxLength = "5000";
-   text.classList.add("text-area");
+        console.log(table);
+        table.classList.add("display-none");
+        page.classList.add("display-none");
 
-   var submit = document.createElement("button");
-   submit.textContent = "submit";
-   submit.classList.add("btn" ,"btn-danger", "margin-right-5");
+         var text = document.createElement("textarea");
+         text.name = "post";
+         text.maxLength = "5000";
+         text.classList.add("text-area");
 
-   var cancel = document.createElement("button");
-   cancel.textContent = "cancel";
-   cancel.classList.add("btn" ,"btn-danger");
+         var submit = document.createElement("button");
+         submit.textContent = "submit";
+         submit.classList.add("btn" ,"btn-danger", "margin-right-5");
 
-
-   function submit_new(){
-     table.classList.remove("display-none");
-     page.classList.remove("display-none");
-     text.classList.add("display-none");
-     submit.classList.add("display-none");
-     cancel.classList.add("display-none");
-     footer.classList.remove("margin-top-450");
-     footer.classList.add("margin-top-50");
-     console.log();
-
-   }
-
-   function recover(){
-     table.classList.remove("display-none");
-     page.classList.remove("display-none");
-     text.classList.add("display-none");
-     submit.classList.add("display-none");
-     cancel.classList.add("display-none");
-     footer.classList.remove("margin-top-450");
-     footer.classList.add("margin-top-50");
-   }
+         var cancel = document.createElement("button");
+         cancel.textContent = "cancel";
+         cancel.classList.add("btn" ,"btn-danger");
 
 
+         function submit_new(){
+           table.classList.remove("display-none");
+           page.classList.remove("display-none");
+           text.classList.add("display-none");
+           submit.classList.add("display-none");
+           cancel.classList.add("display-none");
+
+         }
+
+         function recover(){
+           table.classList.remove("display-none");
+           page.classList.remove("display-none");
+           text.classList.add("display-none");
+           submit.classList.add("display-none");
+           cancel.classList.add("display-none");
+
+         }
 
 
-   submit.addEventListener('click', submit_new);
-   cancel.addEventListener('click', recover);
+         submit.addEventListener('click', submit_new);
+         cancel.addEventListener('click', recover);
 
 
-   news.appendChild(text);
-   news.appendChild(submit);
-   news.appendChild(cancel);
+         news.appendChild(text);
+         news.appendChild(submit);
+         news.appendChild(cancel);
 
 
-  footer.classList.remove("margin-top-50");
-  footer.classList.add("margin-top-450");
+        footer.classList.remove("margin-top-150");
+        footer.classList.add("margin-top-350");
 }
+
+
+/*function edit_products() {
+        var table = document.getElementById("products_table");
+        var products = document.getElementById("products");
+        var page = document.getElementById("pagination");
+        var footer = document.getElementById("footer");
+
+        console.log(table);
+        table.classList.add("display-none");
+        page.classList.add("display-none");
+
+         var text = document.createElement("textarea");
+         text.name = "post";
+         text.maxLength = "5000";
+         text.classList.add("text-area");
+
+         var submit = document.createElement("button");
+         submit.textContent = "submit";
+         submit.classList.add("btn" ,"btn-danger", "margin-right-5");
+
+         var cancel = document.createElement("button");
+         cancel.textContent = "cancel";
+         cancel.classList.add("btn" ,"btn-danger");
+
+
+         function submit_new(){
+           table.classList.remove("display-none");
+           page.classList.remove("display-none");
+           text.classList.add("display-none");
+           submit.classList.add("display-none");
+           cancel.classList.add("display-none");
+
+         }
+
+         function recover(){
+           table.classList.remove("display-none");
+           page.classList.remove("display-none");
+           text.classList.add("display-none");
+           submit.classList.add("display-none");
+           cancel.classList.add("display-none");
+
+         }
+
+
+         submit.addEventListener('click', submit_new);
+         cancel.addEventListener('click', recover);
+
+
+         products.appendChild(text);
+         products.appendChild(submit);
+         products.appendChild(cancel);
+
+
+        footer.classList.remove("margin-top-150");
+        footer.classList.add("margin-top-350");
+}*/
