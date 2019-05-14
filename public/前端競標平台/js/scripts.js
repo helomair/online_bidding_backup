5376073cd53c3b46b5498e1a2a0583743db181f5
mengru <4momolmr@gmail.com>
2019-05-14 11:22:16 +0800

    增加新增公告功能

--- public/前端競標平台/js/scripts.js
+++ public/前端競標平台/js/scripts.js
@@ -1,1 +1,61 @@
-// Empty JS for your own code to be here
+function add_news() {
+  var table = document.getElementById("news_table");
+  var news = document.getElementById("news");
+  var page = document.getElementById("pagination");
+  var footer = document.getElementById("footer");
+  var num = document.getElementById("news_table").rows.length;
+  console.log(table);
+  table.classList.add("display-none");
+  page.classList.add("display-none");
+
+   var text = document.createElement("textarea");
+   text.name = "post";
+   text.maxLength = "5000";
+   text.classList.add("text-area");
+
+   var submit = document.createElement("button");
+   submit.textContent = "submit";
+   submit.classList.add("btn" ,"btn-danger", "margin-right-5");
+
+   var cancel = document.createElement("button");
+   cancel.textContent = "cancel";
+   cancel.classList.add("btn" ,"btn-danger");
+
+
+   function submit_new(){
+     table.classList.remove("display-none");
+     page.classList.remove("display-none");
+     text.classList.add("display-none");
+     submit.classList.add("display-none");
+     cancel.classList.add("display-none");
+     footer.classList.remove("margin-top-450");
+     footer.classList.add("margin-top-50");
+     console.log();
+
+   }
+
+   function recover(){
+     table.classList.remove("display-none");
+     page.classList.remove("display-none");
+     text.classList.add("display-none");
+     submit.classList.add("display-none");
+     cancel.classList.add("display-none");
+     footer.classList.remove("margin-top-450");
+     footer.classList.add("margin-top-50");
+   }
+
+
+
+
+   submit.addEventListener('click', submit_new);
+   cancel.addEventListener('click', recover);
+
+
+   news.appendChild(text);
+   news.appendChild(submit);
+   news.appendChild(cancel);
+
+
+  footer.classList.remove("margin-top-50");
+  footer.classList.add("margin-top-450");
+}
