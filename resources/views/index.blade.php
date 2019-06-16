27cff5d94a3bdc2ab365c418a233265d5d0ee0dc
mengru <4momolmr@gmail.com>
2019-06-16 18:11:09 +0800

    新增公告欄部分 首頁排版調整

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,31 +9,104 @@
 @endsection
 
 @section('content')		  <!-- 多行記錄，後面要加endsection -->
-<div class="container fix_content">
-    <div class="row">
-        <div class="col-12">
-            <h2 align="center">歡迎光臨 {{ env('APP_NAME') }}</h2>
-            <a href=”#”></a>
-        </div>
-		<div class="container">
-			<div class="row space-between">
-			  <div class="card-img">
-				<img src="images/macbook.jpg" class="margin-top-100 mx-auto col-lg-12 ">
-			  </div>
-              @foreach($products as $product)
-              <div class="card-img margin-top-100 col-lg-3">
-                <img src="{{ route('img', $file_path[$product->id]) }}">
-              </div>
-              @endforeach
-              @for ($i = $products->count(); $i < 3; $i++)
-                <div class="card-img margin-top-100 col-lg-3">
-              @endfor 
+		<div class="panel-group margin-top-100" id="accordion">
+			<div class="panel panel-default">
+				<div class="panel-heading">
+					<h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion"
+							 href="#collapseOne">
+							 <div class="container">
+								 <div class="row">
+									<marquee>
+										 <p style="font-family: Impact; font-size: 18pt">測試！測試！123!~~~ 測試！測試！123!~~~ 測試！測試！123!~~~</p>
+									 </marquee>
+								 </div>
+						 </div>
+						</a>
+					</h4>
+				</div>
+				<div id="collapseOne" class="panel-collapse collapse in text-align-center">
+					<div class="panel-body">
+						とろさば料理専門店SABAR（サバー）では、とろさばフルコースや鯖寿司など、<br>「とろさば」の美味しさを世界各国の伝統料理で堪能いただく「とろさば情報発信基地」です。
+					</div>
+				</div>
 			</div>
-        </div>
-		<div class="text-center margin-top-100 col-lg-3 mx-auto">
-            {{ $products->links() }}
-        </div>
-    </div>
-</div>
-</div>
+
+		</div>
+		<div class="container fix_content">
+		        <div class="col-12">
+		            <h2 align="center">歡迎光臨 {{ env('APP_NAME') }}</h2>
+		            <a href=”#”></a>
+		        </div>
+						<div class="card p-3 col-12 col-md-12 col-lg-12 margin-top-100" style="border:1px solid white;">
+								<div class="card-wrapper">
+										<div class="card-img">
+												<a href="">
+													<img src="images/DSCF2069.jpg" alt="Mobirise" title="">
+												</a>
+										</div>
+								</div>
+						</div>
+
+
+						<div class="container">
+								<div class="media-container-row margin-top-100">
+										<div class="card p-3 col-12 col-md-6 col-lg-4" style="border:1px solid white;">
+												<div class="card-wrapper">
+														<div class="card-img">
+																<a href="">
+																	<img src="images/macbook.jpg" alt="Mobirise" title="" >
+																</a>
+														</div>
+
+												</div>
+										</div>
+
+										<div class="card p-3 col-12 col-md-6 col-lg-4" style="border:1px solid white;">
+												<div class="card-wrapper">
+														<div class="card-img">
+																<a href="">
+																	<img src="images/macbook.jpg" alt="Mobirise" title="">
+																</a>
+														</div>
+
+												</div>
+										</div>
+
+										<div class="card p-3 col-12 col-md-6 col-lg-4" style="border:1px solid white;">
+												<div class="card-wrapper">
+														<div class="card-img">
+																<a href="album-3.html">
+																	<img src="images/macbook.jpg" alt="Mobirise" title="">
+																</a>
+														</div>
+
+												</div>
+										</div>
+								</div>
+						</div>
+
+		<!---下面是舊代碼---->
+
+				<!--div class="container">
+					<div class="row space-between">
+					  <div class="card-img">
+						<img src="images/macbook.jpg" class="margin-top-100 mx-auto col-lg-12 ">
+					  </div>
+		              @foreach($products as $product)
+		              <div class="card-img margin-top-100 col-lg-3">
+		                <img src="{{ route('img', $file_path[$product->id]) }}">
+		              </div>
+		              @endforeach
+		              @for ($i = $products->count(); $i < 3; $i++)
+		                <div class="card-img margin-top-100 col-lg-3">
+		              @endfor
+					</div>
+		        </div>
+				<div class="text-center margin-top-100 col-lg-3 mx-auto">
+		            {{ $products->links() }}
+		        </div>
+		    </div-->
+
+		</div>
 @endsection
--- resources/views/layouts/left_button.blade.php
+++ resources/views/layouts/left_button.blade.php
@@ -1,12 +1,12 @@
-<?php 
-$url = URL::current(); 
+<?php
+$url = URL::current();
 $tok_str = strtok($url,"/");
 for($i=0;$i<2;$i++){
 	$tok_str = strtok("/");
 }
 ?>
-<nav class="nav nav-pills nav-fill margin-top-100" style="background-color:#e5f0f7d9;">
-    <a class="nav-item nav-link col-lg-4 <?php if(in_array($tok_str,array("user","bidding"))) echo "active";?>" href="{{ route('user_interface') }}">進行中拍賣</a>
-    <a class="nav-item nav-link col-lg-4 <?php if(in_array($tok_str,array("closed"))) echo "active";?>" href="{{ route('closed') }}">已結標拍賣</a>
-    <a class="nav-item nav-link col-lg-4 <?php if(in_array($tok_str,array("account"))) echo "active";?>" href="{{ route('account') }}">帳戶狀態</a>
+<nav class="nav nav-pills nav-fill margin-top-100 cursor-pointer" >
+    <a class="nav-item nav-link col-lg-4 border-radius-0 <?php if(in_array($tok_str,array("user","bidding"))) echo "active";?>" href="{{ route('user_interface') }}" >進行中拍賣</a>
+    <a class="nav-item nav-link col-lg-4 border-radius-0 <?php if(in_array($tok_str,array("closed"))) echo "active";?>" href="{{ route('closed') }}" >已結標拍賣</a>
+    <a class="nav-item nav-link col-lg-4 border-radius-0 <?php if(in_array($tok_str,array("account"))) echo "active";?>" href="{{ route('account') }}" >帳戶狀態</a>
 </nav>
