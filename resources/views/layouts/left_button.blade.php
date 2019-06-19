9189b4cc82a7806433936c6876ed7c416c494f0e
mengru <4momolmr@gmail.com>
2019-06-19 14:16:04 +0800

    儲值頁面排版

--- resources/views/layouts/left_button.blade.php
+++ resources/views/layouts/left_button.blade.php
@@ -5,7 +5,7 @@ for($i=0;$i<2;$i++){
 	$tok_str = strtok("/");
 }
 ?>
-<nav class="nav nav-pills nav-fill margin-top-100 cursor-pointer" >
+<nav class="nav nav-pills nav-fill margin-top-100 cursor-pointer" style="background-color:#e1f4ff;">
     <a class="nav-item nav-link col-lg-4 border-radius-0 <?php if(in_array($tok_str,array("user","bidding"))) echo "active";?>" href="{{ route('user_interface') }}" >進行中拍賣</a>
     <a class="nav-item nav-link col-lg-4 border-radius-0 <?php if(in_array($tok_str,array("closed"))) echo "active";?>" href="{{ route('closed') }}" >已結標拍賣</a>
     <a class="nav-item nav-link col-lg-4 border-radius-0 <?php if(in_array($tok_str,array("account"))) echo "active";?>" href="{{ route('account') }}" >帳戶狀態</a>
