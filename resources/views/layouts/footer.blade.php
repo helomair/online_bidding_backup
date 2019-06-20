e7ff911666a20dd1264a393eb8ac6460ecef1203
mengru <4momolmr@gmail.com>
2019-06-20 15:15:47 +0800

    footer 版面調整

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,61 +3,46 @@
     <div class="container">
         <div class="media-container-row content text-white">
 
-            <div class="col-12 col-md-9 mbr-fonts-style display-7">
+            <div class="col-12 col-md-9 mbr-fonts-style display-7 text-align-center">
                 <h5 class="pb-3">
-                  聯絡資訊
+                  本系統由以思科技開發 Powered by AS Tech.
                 </h5>
-                <p class="mbr-text">
-                    服務時間：10:00~18:00(週六日除外)
-                    <br><br>
-                    Email : 666666@gmail.com
-                </p>
+
             </div>
 		</div>
         <div class="footer-lower">
             <div class="media-container-row">
 
-                <div class="col-sm-12 row ">
+                <div class="col-lg-12 col-sm-12 row justify-content-center">
                   <hr class="width-100-percent">
-                  <div class="col-1 col-md-1 mbr-fonts-style display-7" >
 
-                    <a href="" class="pb-3">
-
-                    </a>
-                  </div>
-                  <div class="col-2 col-md-2 mbr-fonts-style display-7" >
-
+                  <div class="col-2 footer-rwd mbr-fonts-style display-7 text-align-center" >
                     <a href="{{ route('aboutus') }}" class="pb-3">
                         關於我們
                     </a>
                   </div>
-                  <div class="col-2 col-md-2 mbr-fonts-style display-7" >
+                  <div class="col-2  footer-rwd mbr-fonts-style display-7 text-align-center" >
                     <a href="{{ route('service') }}" class="pb-3">
                         服務條款
                     </a>
                   </div>
-                  <div class="col-2 col-md-2 mbr-fonts-style display-7">
+                  <div class="col-2 footer-rwd mbr-fonts-style display-7 text-align-center">
                     <a href="{{ route('fq') }}" class="pb-3">
                         常見問題
                     </a>
                   </div>
 
-                  <div class="col-2 col-md-2 mbr-fonts-style display-7">
+                  <div class="col-2 footer-rwd mbr-fonts-style display-7 text-align-center">
                     <a href="{{ route('privacy') }}" class="pb-3">
                         隱私條款
                     </a>
                   </div>
-                  <div class="col-2 col-md-2 mbr-fonts-style display-7">
+                  <div class="col-2 footer-rwd mbr-fonts-style display-7 text-align-center">
                     <a href="contact.html" class="pb-3">
                         聯繫我們
                     </a>
                   </div>
-                  <div class="col-1 col-md-1 mbr-fonts-style display-7" >
 
-                    <a href="" class="pb-3">
-
-                    </a>
-                  </div>
 
                 </div>
             </div>
