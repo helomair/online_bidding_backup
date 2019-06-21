367ab77e8aa00ab31e8d005b878a86693c6150ca
mengru <4momolmr@gmail.com>
2019-06-21 23:13:14 +0800

    微調整上次的版

--- resources/views/user/coin.blade.php
+++ resources/views/user/coin.blade.php
@@ -48,16 +48,24 @@
                     <label class="label-300" for="300p">300</label>
                 </div>
             </div>
+
             @if (Auth::user()->recommand_code == NULL)
 			<div class="row col-lg-8 col-md-8  mx-auto margin-top-200">
 			  <h5 class="col-lg-2 add-coin-invite line-height-35" style="font-size:18px;">邀請碼：</h5>
 			   <input type="text" value="" class="col-lg-4  form-control add-coin-input bg-white" name="code" >
 			</div>
             @endif
+						<div class="row col-lg-8 col-md-8  mx-auto margin-top-200">
+							<h5 class="col-lg-2 add-coin-invite line-height-35" style="font-size:18px;">邀請碼：</h5>
+							 <input type="text" value="" class="col-lg-4  form-control add-coin-input bg-white" name="code" >
+						</div>
+			<div class="row col-lg-8 col-md-8  mx-auto margin-top-50">
+				<h5 class="col-lg-6 add-coin-invite line-height-35" style="font-size:18px; min-width:500px!important;">應付金額： <span style="font-size:20px; ">5000元</span></h5>
+			</div>
 			<div class="row col-lg-8 col-md-8 mx-auto margin-top-150" style="justify-content:center;">
 			  <input type="submit" value="提交" class="btn btn-danger padding-05-2 font-size-10" >
 			  <input type="button" value="取消" onclick="window.location='/account'" class="btn btn-danger padding-05-2 font-size-10" >
-            </div>
+      </div>
 		</form>
     </div>
 </div>
