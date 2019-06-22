4240d92b67a9e1ceedb9603366b3b2bae6ab9897
mengru <4momolmr@gmail.com>
2019-06-22 11:44:25 +0800

    商品管理的詳細頁面

--- resources/views/adm/Details.blade.php
+++ resources/views/adm/Details.blade.php
@@ -13,68 +13,66 @@
 	<div class="row">
 			<!--Titles-->
 			<div class="title col-lg-12 margin-top-50">
-					<h2 class="text-align-center mbr-fonts-style display-1 ">
-						Product Contents
+					<h2 class="text-align-center mbr-fonts-style display-5 " style="font-size:40px;">
+						商品詳細
 					</h2>
 			</div>
 
-			<div class="col-lg-6 col-md-6 margin-top-50">
+			<div class="col-lg-6 col-md-6 margin-top-50" style="border-left:6px solid #5a83ce; border-radius:5px;">
 					<div class="left-block wrapper">
 							<div class="row mx-auto my-4">
-									<h5 class="text-align-center mbr-fonts-style display-5 col-md-4 padding-top-30">
+									<h5 class="text-align-center mbr-fonts-style display-1 col-md-4 padding-top-30" style="font-size:25px;">
 											商品名稱：
 									</h5>
-									<span class="col-md-4 text-align-center padding-top-30 display-5">{{$product->name}} </span>
+									<span class="col-md-4 text-align-center padding-top-30 display-1" style="font-size:25px;" >{{$product->name}} </span>
 
 
 							</div>
 
 							<div class="row mx-auto my-4">
-								<h5 class="text-align-center mbr-fonts-style display-5 col-md-4 padding-top-30">
+								<h5 class="text-align-center mbr-fonts-style display-1 col-md-4 padding-top-30" style="font-size:25px;">
 										原價：
 								</h5>
-								<span class="col-md-4 text-align-center padding-top-30 display-5">{{$product->origin_price}}</span>
+								<span class="col-md-4 text-align-center padding-top-30 display-1" style="font-size:25px;">{{$product->origin_price}}</span>
 
 							</div>
 
 							<div class="row mx-auto my-4">
-								<h5 class="text-align-center mbr-fonts-style display-5 col-md-4 padding-top-30">
+								<h5 class="text-align-center mbr-fonts-style display-1 col-md-4 padding-top-30" style="font-size:25px;">
 										起標價：
 								</h5>
-								<span class="col-md-4 text-align-center padding-top-30 display-5">{{$product->cur_cost}}</span>
+								<span class="col-md-4 text-align-center padding-top-30 display-1" style="font-size:25px;">{{$product->cur_cost}}</span>
 
 
 							</div>
 
 							<div class="row mx-auto my-4">
-								<h5 class="text-align-center mbr-fonts-style display-5 col-md-4 padding-top-30">
+								<h5 class="text-align-center mbr-fonts-style display-1 col-md-4 padding-top-30" style="font-size:25px;">
 										公佈時間：
 								</h5>
-								<span class="col-md-4 text-align-center padding-top-30 display-5">{{$product->view_time}}</span>
+								<span class="col-md-4 text-align-center padding-top-30 display-1" style="font-size:25px;"> {{$product->view_time}}</span>
 							</div>
 
 							<div class="row mx-auto my-4">
-								<h5 class="text-align-center mbr-fonts-style display-5 col-md-4 padding-top-30">
+								<h5 class="text-align-center mbr-fonts-style display-1 col-md-4 padding-top-30" style="font-size:25px;">
 										起始時間：
 								</h5>
-								<span class="col-md-4 text-align-center padding-top-30 display-5">{{$product->start_time}}</span>
+								<span class="col-md-4 text-align-center padding-top-30 display-1" style="font-size:25px;">{{$product->start_time}}</span>
 							</div>
 
 							<div class="row mx-auto my-4">
-								<h5 class="text-align-center mbr-fonts-style display-5 col-md-4 padding-top-30">
+								<h5 class="text-align-center mbr-fonts-style display-1 col-md-4 padding-top-30" style="font-size:25px;">
 										結束時間：
 								</h5>
-								<span class="col-md-4 text-align-center padding-top-30 display-5">{{$product->end_time}}</span>
+								<span class="col-md-4 text-align-center padding-top-30 display-1" style="font-size:25px;">{{$product->end_time}}</span>
 
 							</div>
 					</div>
 			</div>
 
 			<div class="col-lg-6 col-md-6 margin-top-50">
-					<h5 class="text-align-center mbr-fonts-style display-5 col-md-12 margin-top-50">
-							簡介
-					</h5>
-					<textarea class="my-4" style="width:100%; height: 350px; border:#000000 1px solid;"></textarea>
+
+					<img src="" style="width:100%; margin-top:50px;"/>
 			</div>
 
 <!--			<div class="row col-md-12 margin-top-50 " style="justify-content: flex-end;">
@@ -85,8 +83,8 @@
 				<hr>
 			</div>
 
-			<div class="title col-lg-12 margin-top-50">
-					<h2 class="text-align-center mbr-fonts-style display-1 font-size-40">
+			<div class="title col-lg-12 margin-top-50" style="border-left:6px solid green; border-radius:5px;">
+					<h2 class="text-align-center mbr-fonts-style display-5 font-size-40">
 					得 標 者 資 訊
 					</h2>
 
