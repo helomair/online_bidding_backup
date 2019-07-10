a493997b05aa68648872b4244ab7427397c99363
mengru <4momolmr@gmail.com>
2019-07-10 11:05:10 +0800

    modify meta

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,10 +4,8 @@
         <meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<meta name="description" content="Source code generated using layoutit.com">
-        <meta name="author" content="LayoutIt!">
-        <meta name="csrf-token" content="{ csrf_token() }}">
-		
+    <meta name="csrf-token" content="{ csrf_token() }}">
+
         <title>@yield('title') | {{env('APP_NAME')}}</title>
 		@yield('css')
 		<link rel="stylesheet" href="{{ asset('assets/web/assets/mobirise-icons/mobirise-icons.css') }}">
@@ -22,10 +20,10 @@
 		<!-- Bootstrap core CSS
 		<link href="{{ asset('vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css') }}" rel="stylesheet">
  -->
-		<!-- Custom styles for this template 
+		<!-- Custom styles for this template
 		<link href="{{ asset('css/shop-item.css') }}" rel="stylesheet">-->
 	</head>
-	
+
     <body>
         @include('layouts.navbar')
         @yield('content')
@@ -35,7 +33,7 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    
+
 		<script src="{{ asset('js/jquery.min.js') }}"></script>
 		<script src="{{ asset('js/scripts.js') }}"></script>
 		<!-- Bootstrap core JavaScript -->
@@ -43,5 +41,5 @@
         <!--<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>-->
     @yield('script')
 	</body>
-	
+
 </html>
