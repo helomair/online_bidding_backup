7baedf369906f1915e58cbf223ab61714bb7c093
helomair <helomairwu@apollosome.com>
2019-07-12 22:45:19 +0700

    modify mail sender to ian

--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'very860112@gmail.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'ian210425@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'Admin'),
     ],
 
